74b434889a8e167caee30c84bd8c8da2a5b5893d
jaybui <buitanphat26@gmail.com>
2023-11-02 21:32:46 -0500

    Added Player Class and Started Setting Up the Game

--- gameofwar.py
+++ gameofwar.py
@@ -3,6 +3,73 @@ suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
+# GAME SETUP
+player_one = Player(One)
+player_two = Player(Two)
+
+new_deck = Deck()
+new_deck.shuffle()
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+game_on = True
+
+round_num = 0
+
+while game_on:
+
+    round_num += 1
+    print(f'Round {round_num}')
+
+    if len(player_one.all_cards) == 0:
+        print('Player One, out of cards! Player Two Wins!')
+        game_on = False
+        break
+    if len(player_two.all_cards) == 0:
+        print('Player Two, out of cards! Player One Wins!')
+        game_on = False
+        break
+
+    # START A NEW ROUND
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+
+    at_war =True
+
+    while at_war:
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+            at_war = False
+
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+            at_war = False
+
+        else:
+            print('WAR!')
+            if len(player_one.all_cards) < 5:
+                print('Player One unable to declare war')
+                print("PlAYER TWO WINS!")
+                game_on = False
+                break
+
+            elif len(player_two.all_cards) < 5:
+                print('Player Two unable to declare war')
+                print("PlAYER ONE WINS!")
+                game_on = False
+                break
+
+            else:
+                player_one_cards.append(player_one.remove_one())
+                player_two_cards.append(player_two.remove_one())
+
 class Card:
     def __init__(self,suit,rank):
         self.suit = suit
@@ -26,3 +93,22 @@ class Deck:
 
     def deal_one(self):
         return self.all_cards.pop()
+    
+class Player:
+    def __init__(self,name):
+        self.name = name
+        self.all_cards = []
+    
+    def remove_one(self):
+        return self.all_cards.pop(0)
+
+    def add_cards(self,new_cards):
+        if type(new_cards) == type([]):
+            # List of multiple Card objects
+            self.all_cards.extend(new_cards)
+        else:
+            # For a single card object
+            self.all_cards.append(new_cards)
+
+    def _str_(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
